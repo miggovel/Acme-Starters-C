c81eb7bfbdede27a712228ad13ac37ca12170c6f
jesmarde2 <jesmarde2@alum.us.es>
2026-03-14 18:13:45 +0100

    Authenticated service (create,update), repo y controller

---
+++ src/main/java/acme/features/authenticated/spokesperson/AuthenticatedSpokespersonController.java
@@ -0,0 +1,24 @@
+
+package acme.features.authenticated.spokesperson;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+
+import acme.client.components.principals.Authenticated;
+import acme.client.controllers.AbstractController;
+import acme.realms.Spokesperson;
+
+@Controller
+public class AuthenticatedSpokespersonController extends AbstractController<Authenticated, Spokesperson> {
+
+	//Constructors -----------------------------------------
+	@PostConstruct
+	protected void initialise() {
+		super.setMediaType(MediaType.TEXT_HTML);
+
+		super.addBasicCommand("create", AuthenticatedSpokespersonCreateService.class);
+		super.addBasicCommand("update", AuthenticatedSpokespersonUpdateService.class);
+	}
+}
